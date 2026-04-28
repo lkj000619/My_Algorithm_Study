4917fb48ee86b8d30c6f196289dea6c1890e74eb
이광진 <35459811+lkj000619@users.noreply.github.com>
2026-04-29 00:00:23 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    // if(num1 == num2){
+    //     return 1;
+    // }
+    // else if(num1 != num2){
+    //     return -1;
+    // }
+    return (num1 == num2)? 1 : -1; 
+}
