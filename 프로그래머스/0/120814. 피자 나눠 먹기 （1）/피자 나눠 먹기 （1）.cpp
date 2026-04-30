a35288f36958c13adf779dd565d984f44baf8b70
이광진 <35459811+lkj000619@users.noreply.github.com>
2026-04-30 13:38:03 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    // int answer = n%7 == 0? n/7 : n/7+1 ;
+    
+    // return answer;
+    
+    return (n%7 == 0)? n/7 : n/7+1;
+}
