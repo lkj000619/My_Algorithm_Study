e1b25e9a97b68d855019878c0589e098f70546d4
이광진 <35459811+lkj000619@users.noreply.github.com>
2026-05-06 08:26:05 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int age) {
+//     int answer = 0;
+//     int year = 2022;
+    
+//     answer = year - (age - 1);
+    
+//     return answer;
+    
+    return 2022 - (age - 1);
+}
