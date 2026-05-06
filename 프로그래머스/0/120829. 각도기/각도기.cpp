335d8b818d95b1d71632092058669d1328e3708d
이광진 <35459811+lkj000619@users.noreply.github.com>
2026-05-06 09:01:06 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+//     int answer = 0;
+//     if(angle > 0 && angle < 90){
+//         answer = 1;
+//     }
+//     else if(angle == 90){
+//         answer = 2;
+//     }
+//     else if(angle > 90 && angle < 180){
+//         answer = 3;
+//     }
+//     else if(angle == 180){
+//         answer = 4;
+//     }
+    
+//     return answer;
+    int answer = (angle > 0 && angle < 90) ?      1 :
+                 angle == 90 ?                    2 : 
+                 (angle > 90 && angle < 180) ?    3 :
+                 angle == 180 ?                   4 :  0;
+    return answer;
+}
