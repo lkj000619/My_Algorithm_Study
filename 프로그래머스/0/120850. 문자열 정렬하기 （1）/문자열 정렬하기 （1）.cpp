080c7a892147705817214df7ffbc1e251ab04668
이광진 <35459811+lkj000619@users.noreply.github.com>
2026-05-13 14:09:20 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer;
+    
+    for(int i = 0; i < my_string.length(); i++){
+        if(my_string[i] >= '0' && my_string[i] <= '9'){
+            int num = my_string[i] - '0';
+            answer.push_back(num);
+        }
+    }
+    
+    sort(answer.begin(), answer.end());
+    
+    return answer;
+}
