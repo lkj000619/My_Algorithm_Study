bb4f469357153e47bf0a1ee48b8bf7f38b9cc465
이광진 <35459811+lkj000619@users.noreply.github.com>
2026-05-19 15:51:29 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 3.93 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    for(int i = 0; i < my_string.length(); i++){
+        if(my_string[i] >= 'a' && my_string[i] <= 'z') answer += my_string[i];
+        else if(my_string[i] >= 'A' && my_string[i] <= 'Z') answer += my_string[i] + 'a' - 'A';
+    }
+    
+    sort(answer.begin(), answer.end());
+    return answer;
+}
